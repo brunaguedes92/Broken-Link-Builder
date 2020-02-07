3702ae5c826814e31f09c5c66f2f73daf7974056
Bruna Guedes <10592020690>
2020-02-07 10:43:34 -0300

    refatorando

--- Example.py
+++ Example.py
@@ -9,14 +9,14 @@ urls = c.getLinksFromURL(os.environ.get('URL'))
 rebasedURLS = c.rebaseURLs(urls)
 filteredInternalURLS = c.filterInternalURLs(rebasedURLS)
 
-#more_urls = c.getLinksFromURLs(filteredInternalURLS)
+more_urls = c.getLinksFromURLs(filteredInternalURLS)
 
-#all_links = c.rebaseURLs(list(set(urls + more_urls)))
+all_links = c.rebaseURLs(list(set(urls + more_urls)))
 
-#urls2 = c.getLinksFromURLs(all_links)
-#all_links2 = c.rebaseURLs(urls2)
+urls2 = c.getLinksFromURLs(all_links)
+all_links2 = c.rebaseURLs(urls2)
 
-url_status_tuple = c.getStatusOfURLs(filteredInternalURLS)
+url_status_tuple = c.getStatusOfURLs(all_links2)
 
 errors = [x for x in url_status_tuple if not x[1] == 200]
 print(errors)
